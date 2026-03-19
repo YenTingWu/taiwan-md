56796ff7eb30c18895ef08cc3b8899872701a3e6
ytw <a9600125a@gmail.com>
2026-03-19 23:24:43 +0800

    feat: setup i18n utils

---
+++ src/i18n/utils.ts
@@ -0,0 +1,14 @@
+import type { Lang } from '../types';
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url: URL): Lang {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as Lang;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  };
+}
